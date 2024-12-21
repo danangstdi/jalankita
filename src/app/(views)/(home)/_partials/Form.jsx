9ed4f8b92ddbcef71502f9711dbb2d7149e97a68
danangstdi <dananginfinity@gmail.com>
2024-12-21 09:02:24 +0700

    3

--- src/app/(views)/(home)/_partials/Form.jsx
+++ src/app/(views)/(home)/_partials/Form.jsx
@@ -124,7 +124,7 @@ export default function Form() {
           />
 
           <div className="grid grid-cols-12">
-            <div className="col-span-2 md:col-span-1 p-3 text-center border-l border-y border-gray-400 bg-gray-100 text-gray-600">
+            <div className="col-span-2 md:col-span-1 p-3 text-center text-sm border-l border-y border-gray-400 bg-gray-100 text-gray-600">
               +62
             </div>
             <input
