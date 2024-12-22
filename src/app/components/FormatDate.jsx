029f1bc3010c7773c8563aca4362939037454658
danangstdi <dananginfinity@gmail.com>
2024-12-23 01:04:12 +0700

    remove time at formatDate

--- src/app/components/FormatDate.jsx
+++ src/app/components/FormatDate.jsx
@@ -1,8 +1,16 @@
-import dayjs from "dayjs";
-import 'dayjs/locale/id';
-
-dayjs.locale('id');
-
 export function FormatDate(dateString) {
-  return dayjs(dateString).format('DD MMMM YYYY, HH:mm');
+  const months = [
+    "Januari", "Februari", "Maret", "April", "Mei", "Juni",
+    "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+  ];
+
+  const date = new Date(dateString);
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+  // const hours = date.getHours().toString().padStart(2, '0');
+  // const minutes = date.getMinutes().toString().padStart(2, '0');
+
+  // return `${day} ${month} ${year}, ${hours}:${minutes}`;
+  return `${day} ${month} ${year}`;
 }
