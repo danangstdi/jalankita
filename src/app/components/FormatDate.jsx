fd80381de8123afb2d32326d5fa5e4f4188ee9da
danangstdi <dananginfinity@gmail.com>
2024-12-23 00:24:16 +0700

    set timezone to jakarta

--- src/app/components/FormatDate.jsx
+++ src/app/components/FormatDate.jsx
@@ -1,15 +1,8 @@
+import dayjs from "dayjs";
+import 'dayjs/locale/id';
+
+dayjs.locale('id');
+
 export function FormatDate(dateString) {
-  const months = [
-    "Januari", "Februari", "Maret", "April", "Mei", "Juni",
-    "Juli", "Agustus", "September", "Oktober", "November", "Desember"
-  ];
-
-  const date = new Date(dateString);
-  const day = date.getDate();
-  const month = months[date.getMonth()];
-  const year = date.getFullYear();
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-
-  return `${day} ${month} ${year}, ${hours}:${minutes}`;
+  return dayjs(dateString).format('DD MMMM YYYY, HH:mm');
 }
