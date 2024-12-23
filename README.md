719a2ea83ea837dbd7cdcdb274496ecbd8d208d4
danangstdi <dananginfinity@gmail.com>
2024-12-23 22:32:59 +0700

    add dicoding:email meta tag

--- README.md
+++ README.md
@@ -2,9 +2,7 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
-1. First, Turn On your apache server for mysql
-
-2. Run Next JS:
+1. Run Next JS:
 ```bash
 npm run dev
 # or
@@ -15,13 +13,15 @@ pnpm dev
 bun dev
 ```
 
-3. Migrate Database:
+2. Masukkan URL Postgre anda di .env DATABASE_URL
+
+2. Migrate Database:
 ```terminal
 npm run migrate
 ```
 
-4. Add superAdmin account in mysql:
-```example: phpmyadmin
+3. Add superAdmin account in postgresql:
+```
 INSERT INTO admins ("id", "adminId", "level", "access", "registerAt", "password", "noEncryptPassword") 
 VALUES (DEFAULT, 'superadmin2024', 'superAdmin', 'ALL', '2024-12-12 11:41:57', '$2a$12$CZ4SGfxKUIsYrWfzh3Je1OTwEQTQKJPJxdbRLYksGQNzXml9IMOFq', 'admin123');
 ```
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,12 +3,28 @@ import NextTopLoader from 'nextjs-toploader';
 
 export const metadata = {
   title: 'JalanKita',
+  applicationName: 'JalanKita',
+  creator: 'Team CS7-F015',
+  keywords: ['JalanKita', 'jalankita', 'Pelaporan Jalan Rusak', 'Web Pelaporan', 'Dicoding Cycle 7'],
   description: 'JalanKita adalah Platorm Layanan Digital Pengaduan Jalan Rusak berbasis web yang dikembangkan oleh beberapa peserta MSIB Batch 7 Dicoding',
+  custom: {
+    email: [
+      'danangstd17@gmail.com',
+      'sachas4f@gmail.com',
+      'jevannandayanasmapan@gmail.com',
+      'wilsondacosta0205@gmail.com',
+    ],
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
+      <head>
+        {metadata.custom.email.map((email) => (
+          <meta key={email} name="dicoding:email" content={email} />
+        ))}
+      </head>
       <body className="poppins bg-slate-200">
         <NextTopLoader color="#10b981" height={5}/>
         {children}
